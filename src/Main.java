ec313681a6c80746b8a6156a7451be26b12a71e4
samclatworthy <samc2027@student.cis.edu.hk>
2024-09-09 09:57:40 +0800

    complete

--- src/Main.java
+++ src/Main.java
@@ -9,8 +9,82 @@
 public class Main {
 
     public static void main(String[] args) {
+        int sumOfTwo = add(5,7);
+        System.out.println("The sum of 5 and 7 is: " + sumOfTwo);
 
+        int sumOfFour = add(5,7,6,6);
+        System.out.println("The sum of 5,7,6 and 6 is: " + sumOfFour);
+
+        String morningGreeting = morningGreeting("Hubert Pong");
+        System.out.println(morningGreeting);
+
+        String afternoonGreeting = afternoonGreeting("Arthur Wong");
+        System.out.println(afternoonGreeting);
+
+        String triple = triple("oohbaby");
+        System.out.println(triple);
+
+        double halfValue = half(19);
+        System.out.println(halfValue);
+
+        int roundedPositiveValue = roundPositiveValueToNearestInteger(69.9);
+        System.out.println(roundedPositiveValue);
+
+        int roundedPositiveValue1 = roundPositiveValueToNearestInteger(7.47);
+        System.out.println(roundedPositiveValue1);
+
+        int roundedNegativeValue2 = roundNegativeValueToNearestInteger(-3.5);
+        System.out.println(roundedNegativeValue2);
+
+        int roundedNegativeValue3 = roundNegativeValueToNearestInteger(-8.5);
+        System.out.println(roundedNegativeValue3);
     }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+    // 1. add
+
+
+    public static int add (int a, int b, int c, int d){
+        return add(a, b) + add(c, d);
+    }
+    // 2. add
+
+
+    public static String morningGreeting(String name){
+        return "早上好, " + name + "!";
+    }
+    // 3. morningGreeting
+
+
+    public static String afternoonGreeting(String name){
+        return "下午好, " + name + "!";
+    }
+    // 4. afternoonGreeting
+
+
+    public static String triple(String s){
+        return s + s + s;
+    }
+    // 5. triple
+
+    public static double half(int n){
+        return n/2.0;
+    }
+    // 6. half
+
+
+    public static int roundPositiveValueToNearestInteger(double d) {
+        return (int) (d + 0.5);
+    }
+    // 7. roundPositiveValueToNearestInteger
+
+    public static int roundNegativeValueToNearestInteger(double d) {
+        return (int) (d - 0.5);
+    }
+
+}
 
     // 1. add
     
@@ -28,4 +102,4 @@ public class Main {
 
     // 8. roundNegativeValueToNearestInteger
 
-}
+
